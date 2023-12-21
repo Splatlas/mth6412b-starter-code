11fabd5fcf69a243581e2a60e2b52e6aebba328d
Jules Caffin <jules.caffin@gmail.com>
2023-12-21 05:57:01 -0500

    Debug write_tour

--- projet/phase4/main.jl
+++ projet/phase4/main.jl
@@ -13,17 +13,18 @@ include("bin/tools.jl")
 
 
 
+
 ## CRÉATION DU FICHIER .tour
-tsp_name = "blue-hour-paris"
-filename = "/Users/jules/Desktop/MTH6412B/Git/mth6412b-starter-code/projet/phase4/tsp/instances/$tsp_name.tsp"
-graphe = build_graph_wo_node0(filename,"Graphe de $tsp_name")
-tour = RSL(graphe)
-tour_entier = V_tour(tour)
-write_tour("test.tour", tour_entier , graph_weight(tour))
+# tsp_name = "blue-hour-paris"
+# filename = "/Users/jules/Desktop/MTH6412B/Git/mth6412b-starter-code/projet/phase4/tsp/instances/$tsp_name.tsp"
+# graphe = build_graph_wo_node0(filename,"Graphe de $tsp_name")
+# # tour = RSL(graphe)
+# # tour_entier = V_tour(tour)
+# write_tour("test.tour", tour_entier , graph_weight(tour))
 
 ## CRÉATION DE L'IMAGE À PARTIR DU FICHIER .tour
-# tour_filename = "/Users/jules/Desktop/MTH6412B/Git/mth6412b-starter-code/test.tour"
-# tour_filename = "/Users/jules/Desktop/MTH6412B/Git/mth6412b-starter-code/projet/phase4/tsp/tours/blue-hour-paris.tour";
-# input_name = "/Users/jules/Desktop/MTH6412B/Git/mth6412b-starter-code/projet/phase4/images/shuffled/blue-hour-paris.png";
-# output_name = "test"
-# reconstruct_picture(tour_filename,input_name,output_name)
+tour_filename = "/Users/jules/Desktop/MTH6412B/Git/mth6412b-starter-code/test.tour"
+#tour_filename = "/Users/jules/Desktop/MTH6412B/Git/mth6412b-starter-code/projet/phase4/tsp/tours/blue-hour-paris.tour";
+input_name = "/Users/jules/Desktop/MTH6412B/Git/mth6412b-starter-code/projet/phase4/images/shuffled/blue-hour-paris.png";
+output_name = "test"
+reconstruct_picture(tour_filename,input_name,output_name)
--- projet/phase4/tools2.jl
+++ projet/phase4/tools2.jl
@@ -8,18 +8,21 @@ include("../phase1/read_stsp.jl")
 Duplicata de build_graph de la phase 1, mais supprime le noeud zéro au moment de la création
 """
 function build_graph_wo_node0(filename::String, graph_name::String)
+    @time begin
     graph_nodes, graph_edges, weights = read_stsp(filename)
+    end
     T = typeof(graph_nodes[1]); #permet d'obtenir le type T
     Y = typeof(weights[1][1]) #permet d'obtenir le type Y
     
     # On crée un graphe vide : composé d'un nom, d'un vecteur de noeuds (les noeuds sont des vecteurs de Float64 qui représentent les coordonnées dans l'espace du noeud (x,y), et d'un vecteur d'arête)
     Graphe = Graph(graph_name, Vector{Node{T}}([]), Vector{Edge{T,Y}}([]))
-    @show 10
     # Ajouter tous les noeuds 
     for i = 1:length(graph_nodes)
         noeud = Node(string(i), graph_nodes[i])  #graph_nodes est un dictionnaire : graph_nodes[i] renvoit le vecteur [x,y] du noeud i
         if !(noeud.data == "1")
             add_node!(Graphe,noeud)
+        elseif noeud.data == "602"
+            @show noeud.data
         end
     end
 
